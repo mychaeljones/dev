eecf1961f5d1239e4a452aec12243a5f864f99a2
mychaeljones <mychael@commoncollective.com>
2017-02-22 13:57:25 -0800

    first version

---
+++ inc_foot.php
@@ -0,0 +1,17 @@
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	include('inc_head.php'); // Contains DOCTYPE, HEAD, BODY, and Beginning of Wrapper
+	include('inc_topbar.php'); // Contains "top-bar": Top Left Navigation & Social Media Links
+	include('inc_header.php'); // Contains "header": Logo & Navigation	
+	include('inc_content.php'); // Contains all content below header and above footer
+	include('inc_footer.php'); // Contains footer content
+	include('inc_foot.php'); // Contains End of Wrapper, Go To Top, and JavaScript Scripts
+?>
+
+		
