5ac13b3155ec1896d3df0d98964689711fdcac0d
mychaeljones <mychael@commoncollective.com>
2017-02-24 17:00:55 -0800

    Posts setup.

--- blog/wp-content/themes/hannahpetsociety/index.php
+++ blog/wp-content/themes/hannahpetsociety/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 		
 		<!-- Content
-		============================================= -->
+		=========================================================== -->
 		<section id="content">
 
 			<div class="content-wrap">
@@ -13,206 +13,59 @@
 				</a>
 
 				<!-- Posts
-				============================================= -->
+				=================================================== -->			
 				<div id="posts" class="post-grid grid-container post-masonry post-masonry-full grid-3 clearfix">
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<a href="images/portfolio/full/17.jpg" data-lightbox="image"><img class="image_fade" src="../../images/blog/small/17.jpg" alt="Standard Post with Image"></a>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single.html">This is a Standard post with a Preview Image</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 10th Feb 2014</li>
-							<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
-							<li><a href="#"><i class="icon-camera-retro"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus.</p>
-							<a href="blog-single.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-				   <div class="entry clearfix">
-						<div class="entry-image">
-							<iframe src="http://player.vimeo.com/video/87701971" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single-full.html">This is a Standard post with an Embedded Video</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 16th Feb 2014</li>
-							<li><a href="blog-single-full.html#comments"><i class="icon-comments"></i> 19</a></li>
-							<li><a href="#"><i class="icon-film"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Asperiores, tenetur, blanditiis, quaerat odit ex exercitationem pariatur quibusdam veritatis quisquam laboriosam esse beatae hic perferendis velit deserunt!</p>
-							<a href="blog-single-full.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<div class="fslider" data-arrows="false" data-lightbox="gallery">
-								<div class="flexslider">
-									<div class="slider-wrap">
-										<div class="slide"><a href="images/portfolio/full/10.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/10.jpg" alt="Standard Post with Gallery"></a></div>
-										<div class="slide"><a href="images/portfolio/full/20.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/20.jpg" alt="Standard Post with Gallery"></a></div>
-										<div class="slide"><a href="images/portfolio/full/21.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/21.jpg" alt="Standard Post with Gallery"></a></div>
-									</div>
+					
+					<!-- The Loop
+					=============================================== -->
+					<?php if(have_posts()) : ?>
+						<?php while(have_posts()) : the_post(); ?>	
+							<div class="entry clearfix">			
+											
+								<!-- Post Image 
+								// Image pop up. Not link 
+								=================================== -->
+								<div class="entry-image">
+									<?php if(has_post_thumbnail()) : ?>
+										<a href="<?php the_post_thumbnail_url('full'); ?>" data-lightbox="image">
+											<img class="image_fade" src="<?php the_post_thumbnail_url(); ?>" alt="Standard Post with Image">
+										</a>
+									<?php endif; ?>
 								</div>
-							</div>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single-small.html">This is a Standard post with a Slider Gallery</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 24th Feb 2014</li>
-							<li><a href="blog-single-small.html#comments"><i class="icon-comments"></i> 21</a></li>
-							<li><a href="#"><i class="icon-picture"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem blanditiis enim culpa reiciendis et explicabo tenetur!</p>
-							<a href="blog-single-small.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<blockquote>
-								<p>"When you are courting a nice girl an hour seems like a second. When you sit on a red-hot cinder a second seems like an hour. That's relativity."</p>
-								<footer>Albert Einstein</footer>
-							</blockquote>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 3rd Mar 2014</li>
-							<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 23</a></li>
-							<li><a href="#"><i class="icon-quote-left"></i></a></li>
-						</ul>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image clearfix">
-							<div class="portfolio-single-image masonry-thumbs col-5" data-big="3" data-lightbox="gallery">
-								<a href="images/portfolio/full/2.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/2.jpg" alt=""></a>
-								<a href="images/portfolio/full/3.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/3.jpg" alt=""></a>
-								<a href="images/portfolio/full/6-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/6-1.jpg" alt=""></a>
-								<a href="images/portfolio/full/6-2.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/6-2.jpg" alt=""></a>
-								<a href="images/portfolio/full/12.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/12.jpg" alt=""></a>
-								<a href="images/portfolio/full/12-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/12-1.jpg" alt=""></a>
-								<a href="images/portfolio/full/13.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/small/13.jpg" alt=""></a>
-							</div>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single-thumbs.html">This is a Standard post with Masonry Thumbs Gallery</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 3rd Mar 2014</li>
-							<li><a href="blog-single-thumbs.html#comments"><i class="icon-comments"></i> 21</a></li>
-							<li><a href="#"><i class="icon-picture"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem blanditiis enim culpa reiciendis et explicabo!</p>
-							<a href="blog-single-thumbs.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<a href="http://themeforest.net" class="entry-link" target="_blank">
-								Themeforest.net
-								<span>- http://themeforest.net</span>
-							</a>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 17th Mar 2014</li>
-							<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 26</a></li>
-							<li><a href="#"><i class="icon-link"></i></a></li>
-						</ul>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<div class="panel panel-default">
-								<div class="panel-body">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, fuga optio voluptatibus saepe tenetur aliquam debitis eos accusantium!
+								
+								<!-- Post Title
+								===================================	-->
+								<div class="entry-title">
+									<h2>
+										<a href="<?php the_permalink(); ?>">
+											<?php the_title(); ?>
+										</a>
+									</h2>
 								</div>
-							</div>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 21st Mar 2014</li>
-							<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 11</a></li>
-							<li><a href="#"><i class="icon-align-justify2"></i></a></li>
-						</ul>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image clearfix">
-							<div class="fslider" data-animation="fade" data-pagi="false" data-pause="6000" data-lightbox="gallery">
-								<div class="flexslider">
-									<div class="slider-wrap">
-										<div class="slide"><a href="images/portfolio/full/6-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/6-1.jpg" alt="Standard Post with Gallery"></a></div>
-										<div class="slide"><a href="images/portfolio/full/6-2.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/6-2.jpg" alt="Standard Post with Gallery"></a></div>
-										<div class="slide"><a href="images/portfolio/full/12-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/12-1.jpg" alt="Standard Post with Gallery"></a></div>
-										<div class="slide"><a href="images/portfolio/full/22.jpg" data-lightbox="gallery-item"><img class="image_fade" src="images/blog/masonry/22.jpg" alt="Standard Post with Gallery"></a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single-thumbs.html">This is a Standard post with Fade Gallery</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 3rd Apr 2014</li>
-							<li><a href="blog-single-thumbs.html#comments"><i class="icon-comments"></i> 21</a></li>
-							<li><a href="#"><i class="icon-picture"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem blanditiis enim culpa reiciendis et explicabo!</p>
-							<a href="blog-single-thumbs.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image clearfix">
-							<iframe width="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/115823769&amp;auto_play=false&amp;hide_related=true&amp;visual=true"></iframe>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single.html">This is an Embedded Audio Post</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 28th April 2014</li>
-							<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 16</a></li>
-							<li><a href="#"><i class="icon-music2"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi autem blanditiis enim culpa reiciendis et explicabo tenetur voluptate rerum.</p>
-							<a href="blog-single.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
-					<div class="entry clearfix">
-						<div class="entry-image">
-							<iframe width="560" height="315" src="http://www.youtube.com/embed/SZEflIVnhH8" frameborder="0" allowfullscreen></iframe>
-						</div>
-						<div class="entry-title">
-							<h2><a href="blog-single-full.html">This is a Standard post with a Youtube Video</a></h2>
-						</div>
-						<ul class="entry-meta clearfix">
-							<li><i class="icon-calendar3"></i> 30th Apr 2014</li>
-							<li><a href="blog-single-full.html#comments"><i class="icon-comments"></i> 34</a></li>
-							<li><a href="#"><i class="icon-film"></i></a></li>
-						</ul>
-						<div class="entry-content">
-							<p>Asperiores, tenetur, blanditiis, quaerat odit ex exercitationem pariatur quibusdam veritatis quisquam laboriosam esse beatae hic perferendis velit deserunt!</p>
-							<a href="blog-single-full.html"class="more-link">Read More</a>
-						</div>
-					</div>
-
+								
+								<!-- Post Meta Data
+								=================================== -->
+								<ul class="entry-meta clearfix">
+									<li><i class="icon-calendar3"></i> <?php the_time('F j, Y'); ?></li>
+									<li><a href="blog-single.html#comments"><i class="icon-comments"></i> 13</a></li>
+									<li><a href="#"><i class="icon-camera-retro"></i></a></li>
+								</ul>
+								
+								<!-- Post Content
+								=================================== -->
+								<div class="entry-content">
+									<p><?php the_excerpt(); ?></p>
+									<a href="blog-single.html"class="more-link">Read More</a>
+								</div>	
+								
+							</div><!-- clearfix end -->				
+						<?php endwhile; ?>
+						
+						<?php else : ?>
+							<p><?php __('No Posts Found'); ?></p>
+					<?php endif; ?>
 				</div><!-- #posts end -->
-
-			</div>
+			</div><!-- .content-wrap end -->
 
 		</section><!-- #content end -->
 
