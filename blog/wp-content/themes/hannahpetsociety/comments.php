4672b4f50e21a43a5fe4e9100204a91b5f81593d
mychaeljones <mychael@commoncollective.com>
2017-02-27 16:38:06 -0800

    Fixed gravatar.

--- blog/wp-content/themes/hannahpetsociety/comments.php
+++ blog/wp-content/themes/hannahpetsociety/comments.php
@@ -19,9 +19,9 @@
 					<div class="comment-author vcard">
 
 						<span class="comment-avatar clearfix">
-						<!-- Comment Avatar -->
-						<?php echo get_avatar( $comment, 32 ); ?>
-						<img alt='' src='http://0.gravatar.com/avatar/ad516503a11cd5ca435acc9bb6523536?s=60' class='avatar avatar-60 photo avatar-default' height='60' width='60' /></span>
+							<!-- Comment Avatar -->
+							<?php echo get_avatar( $comment, 60 ); ?>
+						</span>
 
 					</div>
 
@@ -40,83 +40,13 @@
 				<div class="clear"></div>
 
 			</div>
-<!-- added by me -->
 		</li>
 	<?php
 		}
 	?>
 	</ol>
 		
-<!--
-			<ul class='children'>
 
-				<li class="comment byuser comment-author-_smcl_admin odd alt depth-2" id="li-comment-3">
-
-					<div id="comment-3" class="comment-wrap clearfix">
-
-						<div class="comment-meta">
-
-							<div class="comment-author vcard">
-
-								<span class="comment-avatar clearfix">
-								<img alt='' src='http://1.gravatar.com/avatar/30110f1f3a4238c619bcceb10f4c4484?s=40&amp;d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D40&amp;r=G' class='avatar avatar-40 photo' height='40' width='40' /></span>
-
-							</div>
-
-						</div>
-
-						<div class="comment-content clearfix">
-
-							<div class="comment-author"><a href='#' rel='external nofollow' class='url'>SemiColon</a><span><a href="#" title="Permalink to this comment">April 25, 2012 at 1:03 am</a></span></div>
-
-							<p>Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-
-							<a class='comment-reply-link' href='#'><i class="icon-reply"></i></a>
-
-						</div>
-
-						<div class="clear"></div>
-
-					</div>
-
-				</li>
-
-			</ul>
-
-		</li>
-
-		<li class="comment byuser comment-author-_smcl_admin even thread-odd thread-alt depth-1" id="li-comment-2">
-
-			<div id="comment-2" class="comment-wrap clearfix">
-
-				<div class="comment-meta">
-
-					<div class="comment-author vcard">
-
-						<span class="comment-avatar clearfix">
-						<img alt='' src='http://1.gravatar.com/avatar/30110f1f3a4238c619bcceb10f4c4484?s=60&amp;d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D60&amp;r=G' class='avatar avatar-60 photo' height='60' width='60' /></span>
-
-					</div>
-
-				</div>
-
-				<div class="comment-content clearfix">
-
-					<div class="comment-author"><a href='http://themeforest.net/user/semicolonweb' rel='external nofollow' class='url'>SemiColon</a><span><a href="#" title="Permalink to this comment">April 25, 2012 at 1:03 am</a></span></div>
-
-					<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
-
-					<a class='comment-reply-link' href='#'><i class="icon-reply"></i></a>
-
-				</div>
-
-				<div class="clear"></div>
-
-			</div>
-
-		</li>
-
-	</ol> .commentlist end -->
 
 	<div class="clear"></div>
 
