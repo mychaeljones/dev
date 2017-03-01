afb7f9abf10ff08ec273d50271e819eff4259c8c
mychaeljones <mychael@commoncollective.com>
2017-03-01 08:18:41 -0800

    Changed main content template style

--- inc_content.php
+++ inc_content.php
@@ -1,3 +1,6 @@
+<!-- Hero Unit
+============================================= -->
+
 <section id="slider" class="slider-parallax swiper_wrapper full-screen clearfix">
 	<div class="swiper-container swiper-parent">
 		<div class="swiper-wrapper">
@@ -40,6 +43,7 @@
 
 </section>
 
+
 <!-- Content
 ============================================= -->
 <section id="content">
@@ -48,25 +52,148 @@
 
 		<div class="container clearfix">
 
-			<p><span class="dropcap">F</span>oster best practices effectiveness inspire breakthroughs solve immunize turmoil. Policy dialogue peaceful The Elders rural global support. Process inclusive innovate readiness, public sector complexity. Lifting people up cornerstone partner, technology working families civic engagement activist recognize potential global network. Countries tackling solution respond change-makers tackle. Assistance, giving; fight against malnutrition experience in the field lasting change scalable. Empowerment long-term, fairness policy community progress social responsibility; Cesar Chavez recognition. Expanding community ownership visionary indicator pursue these aspirations accessibility. Achieve; worldwide, life-saving initiative facilitate. New approaches, John Lennon humanitarian relief fundraise vaccine Jane Jacobs community health workers Oxfam. Our ambitions informal economies.</p>
-
-			<blockquote class="topmargin bottommargin">
-				<p>Human rights healthcare immunize; advancement grantees. Medical supplies; meaningful, truth technology catalytic effect. Promising development capacity building international enable poverty.</p>
-			</blockquote>
-
-			<div class="col_half nobottommargin">
-
-				<p>Provide, Aga Khan, interconnectivity governance fairness replicable, new approaches visionary implementation. End hunger evolution, future promising development youth. Public sector, small-scale farmers; harness facilitate gender. Contribution dedicated global change movements, prosperity accelerate progress citizens of change. Elevate; accelerate reduce child mortality; billionaire philanthropy fluctuation, plumpy'nut care opportunity catalyze. Partner deep.</p>
-
+			<div class="col_one_third nobottommargin">
+				<div class="feature-box media-box">
+					<div class="fbox-media">
+						<img src="images/services/1.jpg" alt="Why choose Us?">
+					</div>
+					<div class="fbox-desc">
+						<h3>Why choose Us.<span class="subtitle">Because we are Reliable.</span></h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rem, facilis nobis voluptatum est voluptatem accusamus molestiae eaque perspiciatis mollitia.</p>
+					</div>
+				</div>
 			</div>
 
-			<div class="col_half nobottommargin col_last">
+			<div class="col_one_third nobottommargin">
+				<div class="feature-box media-box">
+					<div class="fbox-media">
+						<img src="images/services/2.jpg" alt="Why choose Us?">
+					</div>
+					<div class="fbox-desc">
+						<h3>Our Mission.<span class="subtitle">To Redefine your Brand.</span></h3>
+						<p>Quos, non, esse eligendi ab accusantium voluptatem. Maxime eligendi beatae, atque tempora ullam. Vitae delectus quia, consequuntur rerum molestias quo.</p>
+					</div>
+				</div>
+			</div>
 
-				<p>Frontline harness criteria governance freedom contribution. Campaign Angelina Jolie natural resources, Rockefeller peaceful philanthropy human potential. Justice; outcomes reduce carbon emissions nonviolent resistance human being. Solve innovate aid communities; benefit truth rural development UNICEF meaningful work. Generosity Action Against Hunger relief; many voices impact crisis situation poverty pride. Vaccine carbon.</p>
+			<div class="col_one_third nobottommargin col_last">
+				<div class="feature-box media-box">
+					<div class="fbox-media">
+						<img src="images/services/3.jpg" alt="Why choose Us?">
+					</div>
+					<div class="fbox-desc">
+						<h3>What we Do.<span class="subtitle">Make our Customers Happy.</span></h3>
+						<p>Porro repellat vero sapiente amet vitae quibusdam necessitatibus consectetur, labore totam. Accusamus perspiciatis asperiores labore esse ab accusantium ea modi ut.</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="section">
+
+			<div class="container clearfix">
+
+				<div class="heading-block center">
+					<h2>Easy Configurable Options.</h2>
+					<span>Choose from a wide array of Options for your best matched Customizations</span>
+				</div>
+
+				<div class="col_full">
+
+					<img data-animate="fadeIn" class="aligncenter" src="images/services/macbook.png" alt="Macbook">
+
+				</div>
+
+				<div class="col_one_third nobottommargin">
+
+					<div class="feature-box fbox-plain">
+						<div class="fbox-icon">
+							<a href="#"><i class="i-alt">1.</i></a>
+						</div>
+						<h3>Choose a Product.</h3>
+						<p>Perferendis, nam. Eum aperiam vel animi beatae corporis dignissimos, molestias, placeat, maxime optio ipsam nostrum atque quidem.</p>
+					</div>
+
+				</div>
+
+				<div class="col_one_third nobottommargin">
+
+					<div class="feature-box fbox-plain">
+						<div class="fbox-icon">
+							<a href="#"><i class="i-alt">2.</i></a>
+						</div>
+						<h3>Enter Shipping Info.</h3>
+						<p>Saepe qui enim at animi. Repellendus, blanditiis doloremque asperiores reprehenderit deleniti. Ipsam nam accusantium ex!</p>
+					</div>
+
+				</div>
+
+				<div class="col_one_third nobottommargin col_last">
+
+					<div class="feature-box fbox-plain">
+						<div class="fbox-icon">
+							<a href="#"><i class="i-alt">3.</i></a>
+						</div>
+						<h3>Complete your Payment.</h3>
+						<p>Necessitatibus accusamus, inventore atque commodi, animi architecto ea sed, suscipit tempora ex deleniti quae. Consectetur, sint velit.</p>
+					</div>
+
+				</div>
 
 			</div>
 
 		</div>
+
+		<div class="container clearfix">
+
+			<div class="col_two_fifth topmargin nobottommargin" id="radarChart" style="opacity: 0;">
+				<canvas id="radarChartCanvas" width="430" height="300"></canvas>
+			</div>
+
+			<div class="col_three_fifth nobottommargin col_last">
+
+				<div class="heading-block">
+					<h3>Powerful insights to help grow your business.</h3>
+					<span>Reports let you easily track and analyze your product sales, orders, and payments.</span>
+				</div>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quod consequuntur quibusdam, enim expedita sed quia nesciunt incidunt accusamus necessitatibus modi adipisci officia libero accusantium esse hic, obcaecati, ullam, laboriosam!</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti vero, animi suscipit id facere officia. Aspernatur, quo, quos nisi dolorum aperiam fugiat deserunt velit rerum laudantium cum magnam excepturi quod, fuga architecto provident, cupiditate delectus voluptate eaque! Sit neque ut eum, voluptatibus odit cum dolorum ipsa voluptates inventore cumque a.</p>
+
+				<a href="#">Learn more &rarr;</a>
+
+			</div>
+
+			<div class="clear"></div><div class="line"></div>
+
+			<div class="col_three_fifth">
+
+				<div class="heading-block">
+					<h3>Advanced store statistics.</h3>
+					<span>Benchmarking your website's performance helps you make great choices for your business.</span>
+				</div>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quod consequuntur quibusdam, enim expedita sed quia nesciunt incidunt accusamus necessitatibus modi adipisci officia libero accusantium esse hic, obcaecati, ullam, laboriosam!</p>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti vero, animi suscipit id facere officia. Aspernatur, quo, quos nisi dolorum aperiam fugiat deserunt velit rerum laudantium cum magnam excepturi quod, fuga architecto provident, cupiditate delectus voluptate eaque! Sit neque ut eum, voluptatibus odit cum dolorum ipsa voluptates inventore cumque a.</p>
+
+				<a href="#">Learn more &rarr;</a>
+
+			</div>
+
+			<div class="col_two_fifth topmargin col_last" id="doughnutChart" style="opacity: 0;">
+				<canvas id="doughnutChartCanvas" width="430" height="300"></canvas>
+			</div>
+
+		</div>
+
+		<a class="button button-full center tright topmargin footer-stick">
+			<div class="container clearfix">
+				Need help with your Account? <strong>Start here</strong> <i class="icon-caret-right" style="top:4px;"></i>
+			</div>
+		</a>
 
 	</div>
 
