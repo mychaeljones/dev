7d080625196a258d5be8e04adaf5bad0a196ddc4
mychaeljones <mychael@commoncollective.com>
2017-03-06 10:10:19 -0800

    integrated blog stylings with global site stylesheet for easier style management

--- inc_foot.php
+++ inc_foot.php
@@ -1,3 +1,22 @@
+<?php if(check_for_blog()) : ?>
+		
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../../js/jquery.js"></script>
+	<script type="text/javascript" src="../../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../../js/functions.js"></script>
+
+
+<?php else : ?>
 	</div><!-- #wrapper end -->
 
 	<!-- Go To Top
@@ -85,5 +104,9 @@
 
 	</script>
 
+<?php endif; ?>
+
 </body>
 </html>
+
+	
