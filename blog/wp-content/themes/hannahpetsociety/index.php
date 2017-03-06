dd0f427be50c50919a2aa9183052e206af076835
mychaeljones <mychael@commoncollective.com>
2017-03-06 10:10:10 -0800

    integrated blog stylings with global site stylesheet for easier style management

--- blog/wp-content/themes/hannahpetsociety/index.php
+++ blog/wp-content/themes/hannahpetsociety/index.php
@@ -6,13 +6,14 @@
 
 	<div class="content-wrap">
 
-<!--
+
 		<a class="button button-full button-purple center tright header-stick bottommargin-lg">
 			<div class="container clearfix">
-				Canvas comes with Unlimited Customizations &amp; Options. <strong>Check Out</strong> <i class="icon-caret-right" style="top:4px;"></i>
+
+				
 			</div>
 		</a>
--->
+
 
 		<div class="container clearfix">
 			<!-- Posts
