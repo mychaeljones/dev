82d38fedccdcd4827da01aa9888374326164db03
mychaeljones <mychael@commoncollective.com>
2017-03-09 15:59:49 -0800

    added pinterest

--- inc_topbar.php
+++ inc_topbar.php
@@ -1,3 +1,5 @@
+<?php echo $url; // for dev purposes ?>
+
 <!-- Top Bar
 ============================================= -->
 <div id="top-bar">
@@ -44,10 +46,12 @@
 				<ul>
 					<li><a href="<?php social("facebook"); ?>" class="si-facebook"><span class="ts-icon"><i class="icon-facebook"></i></span><span class="ts-text">Facebook</span></a></li>
 					<li><a href="<?php social("twitter"); ?>" class="si-twitter"><span class="ts-icon"><i class="icon-twitter"></i></span><span class="ts-text">Twitter</span></a></li>
-<!-- 					<li><a href="#" class="si-dribbble"><span class="ts-icon"><i class="icon-dribbble"></i></span><span class="ts-text">Dribbble</span></a></li>
+<!--
+ 					<li><a href="#" class="si-dribbble"><span class="ts-icon"><i class="icon-dribbble"></i></span><span class="ts-text">Dribbble</span></a></li>
 					<li><a href="#" class="si-github"><span class="ts-icon"><i class="icon-github-circled"></i></span><span class="ts-text">Github</span></a></li>
-					<li><a href="#" class="si-pinterest"><span class="ts-icon"><i class="icon-pinterest"></i></span><span class="ts-text">Pinterest</span></a></li>
--->					<li><a href="<?php social("instagram"); ?>" class="si-instagram"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
+-->
+					<li><a href="<?php social("pinterest"); ?>" class="si-pinterest"><span class="ts-icon"><i class="icon-pinterest"></i></span><span class="ts-text">Pinterest</span></a></li>
+					<li><a href="<?php social("instagram"); ?>" class="si-instagram"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
 					<li><a href="tel:+91.11.85412542" class="si-call"><span class="ts-icon"><i class="icon-call"></i></span><span class="ts-text">+91.11.85412542</span></a></li>
 					<li><a href="mailto:info@canvas.com" class="si-email3"><span class="ts-icon"><i class="icon-email3"></i></span><span class="ts-text">info@canvas.com</span></a></li>
 				</ul>
