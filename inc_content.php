5c44973d5e3ee5d46dee4057dc780cf2a2566099
mychaeljones <mychael@commoncollective.com>
2017-03-14 16:54:07 -0700

    removed unnecessary div which fixed parallax

--- inc_content.php
+++ inc_content.php
@@ -1,45 +1,78 @@
+<style>
+	#hpSlider {
+		/* background-color: rgba(255,255,255,.8);*/
+	    -webkit-box-shadow: none;
+	    box-shadow: none;
+	    border: none;
+	    padding-left:20px;		
+	}
+</style>
+
 <!-- Hero Unit
 ============================================= -->
 
-<section id="slider" class="slider-parallax swiper_wrapper full-screen clearfix">
-	<div class="swiper-container swiper-parent">
-		<div class="swiper-wrapper">
-			<div class="swiper-slide dark" style="background-image: url('images/slider/swiper/1.jpg');">
-				<div class="container clearfix">
-					<div class="slider-caption slider-caption-center">
-						<h2 data-caption-animate="fadeInUp">Welcome to Canvas</h2>
-						<p data-caption-animate="fadeInUp" data-caption-delay="200">Create just what you need for your Perfect Website. Choose from a wide range of Elements &amp; simply put them on our Canvas.</p>
+<section id="slider" class="slider-parallax swiper_wrapper clearfix">
+		<div class="swiper-container swiper-parent">
+			<div class="swiper-wrapper">
+				<!-- Slide 1 -->
+				<div class="swiper-slide dark" style="background-image: url('images/slider/swiper/1.jpg'); background-size:cover;">
+					<div class="container clearfix">
+						<div class="slider-caption slider-caption-right">
+							<h2 data-caption-animate="fadeInUp">
+								No Risk. 
+								<span style = "display: block; font-size: .4em;">
+								Plus a lifetime of no worries.
+								</span>
+							</h2>
+							<p data-caption-animate="fadeInUp" data-caption-delay="200" style="font-size: 1em;">
+							   	Attend a FREE comprehensive vet exam for your Pet.
+							</p>
+							<p data-caption-animate="fadeInUp" data-caption-delay="400" style="font-size: 1em;">								  
+							   	Receive your Pet's monthly pricing.
+							</p>
+							<p data-caption-animate="fadeInUp" data-caption-delay="600" style="font-size: 1em;">								  
+								Sign up for lifetime.							
+							</p>	   	
+							<p data-caption-animate="fadeInUp" data-caption-delay="800" style="font-size: 1em;">								  
+						   		Pay your first monthly fee.
+							</p>
+							<p data-caption-animate="fadeInUp" data-caption-delay="1000" style="font-size: 1em;">								  
+								Start enjoying your Hannah Membership!
+							</p>					
+						</div>
+					</div>
+				</div>
+				<!-- Slide 2 -->
+				<div class="swiper-slide dark">
+					<div class="container clearfix">
+						<div class="slider-caption slider-caption-center">
+							<h2 data-caption-animate="fadeInUp">Beautifully Flexible</h2>
+							<p data-caption-animate="fadeInUp" data-caption-delay="200">Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Powerful Layout with Responsive functionality that can be adapted to any screen size.</p>
+						</div>
+					</div>
+					<div class="video-wrap">
+						<video poster="images/videos/explore.jpg" preload="auto" loop autoplay muted>
+							<source src='images/videos/explore.mp4' type='video/mp4' />
+							<source src='images/videos/explore.webm' type='video/webm' />
+						</video>
+						<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+					</div>
+				</div>
+				<!-- Slide 3 -->
+				<div class="swiper-slide" style="background-image: url('images/slider/swiper/3.jpg'); background-position: center top;">
+					<div class="container clearfix">
+						<div class="slider-caption">
+							<h2 data-caption-animate="fadeInUp">Great Performance</h2>
+							<p data-caption-animate="fadeInUp" data-caption-delay="200">You'll be surprised to see the Final Results of your Creation &amp; would crave for more.</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="swiper-slide dark">
-				<div class="container clearfix">
-					<div class="slider-caption slider-caption-center">
-						<h2 data-caption-animate="fadeInUp">Beautifully Flexible</h2>
-						<p data-caption-animate="fadeInUp" data-caption-delay="200">Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Powerful Layout with Responsive functionality that can be adapted to any screen size.</p>
-					</div>
-				</div>
-				<div class="video-wrap">
-					<video poster="images/videos/explore.jpg" preload="auto" loop autoplay muted>
-						<source src='images/videos/explore.mp4' type='video/mp4' />
-						<source src='images/videos/explore.webm' type='video/webm' />
-					</video>
-					<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
-				</div>
-			</div>
-			<div class="swiper-slide" style="background-image: url('images/slider/swiper/3.jpg'); background-position: center top;">
-				<div class="container clearfix">
-					<div class="slider-caption">
-						<h2 data-caption-animate="fadeInUp">Great Performance</h2>
-						<p data-caption-animate="fadeInUp" data-caption-delay="200">You'll be surprised to see the Final Results of your Creation &amp; would crave for more.</p>
-					</div>
-				</div>
-			</div>
+			<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
+			<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
+			<div id="slide-number"><div id="slide-number-current"></div><span>/</span><div id="slide-number-total"></div></div>
 		</div>
-		<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
-		<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
-		<div id="slide-number"><div id="slide-number-current"></div><span>/</span><div id="slide-number-total"></div></div>
-	</div>
+
 
 </section>
 
@@ -58,8 +91,8 @@
 						<img src="images/services/1.jpg" alt="Why choose Us?">
 					</div>
 					<div class="fbox-desc">
-						<h3>Why choose Us.<span class="subtitle">Because we are Reliable.</span></h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi rem, facilis nobis voluptatum est voluptatem accusamus molestiae eaque perspiciatis mollitia.</p>
+						<h3>Tell us about your pet.<span class="subtitle">Because we are Reliable.</span></h3>
+						<p>Provide your Pet's name, age, breed and current health status to receive a quote.</p>
 					</div>
 				</div>
 			</div>
@@ -70,8 +103,8 @@
 						<img src="images/services/2.jpg" alt="Why choose Us?">
 					</div>
 					<div class="fbox-desc">
-						<h3>Our Mission.<span class="subtitle">To Redefine your Brand.</span></h3>
-						<p>Quos, non, esse eligendi ab accusantium voluptatem. Maxime eligendi beatae, atque tempora ullam. Vitae delectus quia, consequuntur rerum molestias quo.</p>
+						<h3>Schedule a free vet exam.<span class="subtitle">To Redefine your Brand.</span></h3>
+						<p>A Membership Coordinator will contact you within 12 hours to schedule a FREE veterinary exam and behavior evaluation (valued at $400).</p>
 					</div>
 				</div>
 			</div>
@@ -82,8 +115,8 @@
 						<img src="images/services/3.jpg" alt="Why choose Us?">
 					</div>
 					<div class="fbox-desc">
-						<h3>What we Do.<span class="subtitle">Make our Customers Happy.</span></h3>
-						<p>Porro repellat vero sapiente amet vitae quibusdam necessitatibus consectetur, labore totam. Accusamus perspiciatis asperiores labore esse ab accusantium ea modi ut.</p>
+						<h3>Customize your plan.<span class="subtitle">Make our Customers Happy.</span></h3>
+						<p>Option to add Food Delivery, Parasite Control or Grooming to your plan.</p>
 					</div>
 				</div>
 			</div>
@@ -91,7 +124,7 @@
 		</div>
 
 		<div class="section">
-
+<!--
 			<div class="container clearfix">
 
 				<div class="heading-block center">
@@ -145,6 +178,7 @@
 
 		</div>
 
+
 		<div class="container clearfix">
 
 			<div class="col_two_fifth topmargin nobottommargin" id="radarChart" style="opacity: 0;">
@@ -188,13 +222,108 @@
 			</div>
 
 		</div>
+-->
 
+<!-- Member Testimonials
+============================================= -->
+	
+	<div class="container clearfix">
+		<h3 class="center">Member Testimonials</h3>
+
+		<div id="oc-testi" class="owl-carousel testimonials-carousel carousel-widget" data-margin="20" data-items-xs="1" data-items-sm="2" data-items-lg="3">
+
+			<div class="oc-item">
+				<div class="testimonial">
+					<div class="testi-image">
+						<a href="#"><img src="images/testimonials/1.jpg" alt="Customer Testimonails"></a>
+					</div>
+					<div class="testi-content">
+						<p>Hannah gives me the peace of mind I need for ensuring I can give my dogs the longest happiest life possible. Hannah partners with me on the wellness of my dogs and I am always in charge with decision making. I can afford 4 dogs because of Hannah. One of my dogs is 15 years old and Hannah has extended his life several times. I cannot imagine owning my dogs without the innovative Hannah pet care business model.</p>
+						<div class="testi-meta">
+							Johanny
+							<span>Hannah Member since May 2013</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="oc-item">
+				<div class="testimonial">
+					<div class="testi-image">
+						<a href="#"><img src="images/testimonials/2.jpg" alt="Customer Testimonails"></a>
+					</div>
+					<div class="testi-content">
+						<p>I have had two Hannah pets for 3 years now and I am so appreciative to have their partnership in caring for my special family members. The staff at Westside is so caring and friendly. I have never for a minute been sorry that I chose to partner with Hannah the Pet Society!</p>
+						<div class="testi-meta">
+							Dorene
+							<span>Hannah Member since May 2013</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="oc-item">
+				<div class="testimonial">
+					<div class="testi-image">
+						<a href="#"><img src="images/testimonials/7.jpg" alt="Customer Testimonails"></a>
+					</div>
+					<div class="testi-content">
+						<p>Fugit officia dolor sed harum excepturi ex iusto magnam asperiores molestiae qui natus obcaecati facere sint amet.</p>
+						<div class="testi-meta">
+							Mary Jane
+							<span>Google Inc.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="oc-item">
+				<div class="testimonial">
+					<div class="testi-image">
+						<a href="#"><img src="images/testimonials/3.jpg" alt="Customer Testimonails"></a>
+					</div>
+					<div class="testi-content">
+						<p>Similique fugit repellendus expedita excepturi iure perferendis provident quia eaque. Repellendus, vero numquam?</p>
+						<div class="testi-meta">
+							Steve Jobs
+							<span>Apple Inc.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="oc-item">
+				<div class="testimonial">
+					<div class="testi-image">
+						<a href="#"><img src="images/testimonials/4.jpg" alt="Customer Testimonails"></a>
+					</div>
+					<div class="testi-content">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, perspiciatis illum totam dolore deleniti labore.</p>
+						<div class="testi-meta">
+							Jamie Morrison
+							<span>Adobe Inc.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<!-- Member Testimonials
+============================================= -->
 		<a class="button button-full center tright topmargin footer-stick">
 			<div class="container clearfix">
 				Need help with your Account? <strong>Start here</strong> <i class="icon-caret-right" style="top:4px;"></i>
 			</div>
 		</a>
 
+
+
 	</div>
+</div>
+
+
 
 </section><!-- #content end -->
