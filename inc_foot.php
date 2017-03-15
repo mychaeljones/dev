ddab39f212e3115755203ea46bb8e8b67911623a
mychaeljones <mychael@commoncollective.com>
2017-03-15 09:58:34 -0700

    removed redundant calls to js functions.

--- inc_foot.php
+++ inc_foot.php
@@ -32,10 +32,6 @@
 	============================================= -->
 	<script type="text/javascript" src="js/functions.js"></script>
 	
-		<!-- Footer Scripts
-	============================================= -->
-	<script type="text/javascript" src="js/functions.js"></script>
-
 	<script type="text/javascript">
 
 		jQuery(window).load( function(){
