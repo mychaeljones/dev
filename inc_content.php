5c07a3ff6a3c502a4e418b49b9b8b1ee701b8aa4
mychaeljones <mychael@commoncollective.com>
2017-03-14 17:14:08 -0700

    added parallax counter

--- inc_content.php
+++ inc_content.php
@@ -320,9 +320,42 @@
 				Need help with your Account? <strong>Start here</strong> <i class="icon-caret-right" style="top:4px;"></i>
 			</div>
 		</a>
+	</div>
+	
+	
+	
+<!-- Counter in Parallax
+============================================= -->	
+	<div class="section parallax" style="background: url('images/parallax/3.jpg') center center; background-size:cover; padding: 100px 0;" data-stellar-background-ratio="0.3">
+		<div class="container clearfix">
+	
+								<div class="col_one_fourth nobottommargin center">
+						<i class="i-plain i-xlarge divcenter nobottommargin icon-diamond"></i>
+						<div class="counter counter-large" style="color: #1abc9c;"><span data-from="100" data-to="8465" data-refresh-interval="50" data-speed="2000"></span></div>
+						<h5>Clients Served</h5>
+					</div>
+
+					<div class="col_one_fourth nobottommargin center">
+						<i class="i-plain i-xlarge divcenter nobottommargin icon-globe"></i>
+						<div class="counter counter-large" style="color: #e74c3c;"><span data-from="100" data-to="56841" data-refresh-interval="50" data-speed="2500"></span></div>
+						<h5>Lines of Code</h5>
+					</div>
+
+					<div class="col_one_fourth nobottommargin center">
+						<i class="i-plain i-xlarge divcenter nobottommargin icon-gift"></i>
+						<div class="counter counter-large" style="color: #3498db;"><span data-from="100" data-to="2154" data-refresh-interval="50" data-speed="3500"></span></div>
+						<h5>No. of Projects</h5>
+					</div>
+
+					<div class="col_one_fourth nobottommargin center col_last">
+						<i class="i-plain i-xlarge divcenter nobottommargin icon-fire"></i>
+						<div class="counter counter-large" style="color: #9b59b6;"><span data-from="100" data-to="874" data-refresh-interval="30" data-speed="2700"></span></div>
+						<h5>Cups of Coffee</h5>
+					</div>
 
 
-
+	
+		</div>
 	</div>
 </div>
 
